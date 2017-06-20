74763569b93cbcb5f351ebd337c5e6719e7bebb3
Kalle Lindström <karl@dewire.com>
2017-06-20 09:50:59 +0200

    Added getVisibleViewController method

---
+++ SwiftCommon/UIView.swift
@@ -0,0 +1,36 @@
+//
+//  UIView.swift
+//  SwiftCommon
+//
+//  Created by Kalle Lindström on 2017-06-20.
+//  Copyright © 2017 Dewire. All rights reserved.
+//
+
+import UIKit
+
+extension UIWindow {
+  
+  /**
+   Returns the current visible view controller or nil starting the search from the window's root view controller.
+   */
+  var visibleViewController: UIViewController? {
+    return UIWindow.getVisibleViewController(from: self.rootViewController)
+  }
+  
+  /**
+   Returns the current visible view controller or nil starting the search from the given view controller.
+   */
+  static func getVisibleViewController(from vc: UIViewController?) -> UIViewController? {
+    if let nc = vc as? UINavigationController {
+      return UIWindow.getVisibleViewController(from: nc.visibleViewController)
+    } else if let tc = vc as? UITabBarController {
+      return UIWindow.getVisibleViewController(from: tc.selectedViewController)
+    } else {
+      if let pvc = vc?.presentedViewController {
+        return UIWindow.getVisibleViewController(from: pvc)
+      } else {
+        return vc
+      }
+    }
+  }
+}
